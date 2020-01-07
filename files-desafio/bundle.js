9d83ba5ba2ee8cfe58f47a1592e05ce4d936e02b
André Alves <roy_leavs@outlook.com>
2020-01-07 11:49:22 -0300

    Fim.
    Acredito que as propostas foram cumpridas, com uma pequena exceção no border-radius da caixa, que deixei 0 por não criar um efeito muito bonito.
    Ademais. todas os inputs estão funcionais e com seu valores guardados.

--- files-desafio/bundle.js
+++ files-desafio/bundle.js
@@ -4,38 +4,50 @@
 var fundoInp = document.querySelector('#numStick');
 
 function add(valorMaximo) {
-  var value = parseInt(document.querySelector('#numStick').value);
-  value = isNaN(value) ? 0 : value;
+	var value = parseInt(document.querySelector('#numStick').value);
+	value = isNaN(value) ? 0 : value;
 
-  if (value >= valorMaximo) {
-    value = valorMaximo;
-  } else {
-    value++;
-    fundoInp.style.backgroundColor = '#dde3e9';
-    fundoInp.style.border = '1px solid #2f3676';
-  }
+	if (value >= valorMaximo) {
+		value = valorMaximo;
+	} else {
+		value++;
+		fundoInp.style.backgroundColor = '#dde3e9';
+		fundoInp.style.border = '1px solid #2f3676';
+	}
 
-  document.querySelector('#numStick').value = value;
+	document.querySelector('#numStick').value = value;
 }
 
 function sub(valorMinimo) {
-  var value = parseInt(document.querySelector('#numStick').value, 0);
-  value = isNaN(value) ? 0 : value;
+	var value = parseInt(document.querySelector('#numStick').value, 0);
+	value = isNaN(value) ? 0 : value;
 
-  if (value <= valorMinimo) {
-    value = valorMinimo;
-    fundoInp.style.backgroundColor = '#F59393';
-    fundoInp.style.border = '1px solid red';
-  } else {
-    value--;
-  }
+	if (value <= valorMinimo) {
+		value = valorMinimo;
+		fundoInp.style.backgroundColor = '#F59393';
+		fundoInp.style.border = '1px solid red';
+	} else {
+		value--;
+	}
 
-  document.querySelector('#numStick').value = value;
+	document.querySelector('#numStick').value = value;
 }
 
 function resultado() {
-  var chkReact = document.querySelector('#chkReact').checked;
-  var chkVue = document.querySelector('#chkVue').checked;
-  var chkAngular = document.querySelector('#chkAngular').checked;
-  var disgrasa = [chkReact, chkVue, chkAngular];
+	var valor = document.querySelector('#numStick').value;
+	var obs = document.querySelector('#observe').value;
+	var chkReact = document.querySelector('#chkReact');
+	var chkVue = document.querySelector('#chkVue');
+	var chkAngular = document.querySelector('#chkAngular');
+	var arr = [chkReact, chkVue, chkAngular];
+	var marcados = arr.filter(function (item) {
+		return item.checked === true;
+	});
+	var nomes = marcados.map(function (item) {
+		return item.name;
+	});
+	console.log('Você pediu ' + valor + ' sticker(s) de ' + nomes);
+	console.log('Com as seguintes observações: ' + obs);
+	alert('Você pediu ' + valor + ' sticker(s) de ' + nomes.join('/ '));
+	alert('Com as seguintes observações: ' + obs);
 }
